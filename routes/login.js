2251026c22faf209be1cb53c99417b027b8fcc36
momanyisam <momanyisamuel48@gmail.com>
2019-06-23 06:49:49 +0300

    version one

--- routes/login.js
+++ routes/login.js
@@ -13,6 +13,15 @@ router.post('/login',(req,res,next) => {
     })(req,res,next);
 });
 
+router.get("/random", (req, res) => res.render('login/register',{layout: false}));
+
+router.post('/random',(req,res,next) => {
+  console.log(req.body)
+  let { firstName,lastName,email,userStatus,password } = req.body
+  Models.User.create({firstName,lastName,email,userStatus,password})
+  .then((user) => {res.send({message: user})})
+});
+
 router.get('/logout', function(req, res){
     req.logout();
     res.redirect('/');
